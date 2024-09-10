ab314ed660392b29ee6098f465917b8f84950ff7
Ganesh Ravaloji <111950575+GaneshRavaloji@users.noreply.github.com>
2024-09-10 19:54:22 +0530

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test6 {
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+        driver.get(" https://www.getcalley.com/page-sitemap.xml");
+        driver.findElement(By.linkText("https://www.getcalley.com/")).click();
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(375,667));
+        File screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot,new File("C:\\Users\\susmi\\eclipse-workspace\\DemoProject\\Mobile/screenshot3.png"));
+        driver.quit();
+	}
+}
